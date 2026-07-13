8f33eb73e0f14794c9c63d3142ed03b4aa314831
samanvaya <samanvayavats@gmail.com>
2026-07-13 22:42:48 +0530

    added the sign-in page

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SignInPage from '@/src/components/ui/sign-up-page'
+const page = () => {
+  return (
+    <div>
+      <SignInPage type = {false}/>
+    </div>
+  )
+}
+
+export default page
