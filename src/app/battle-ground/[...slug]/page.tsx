c55b2d12f14556a14a1146ccf6b31e9796ca9c16
samanvaya <samanvayavats@gmail.com>
2026-08-18 22:54:42 +0530

    removed the unecessary logs

--- src/app/battle-ground/[...slug]/page.tsx
+++ src/app/battle-ground/[...slug]/page.tsx
@@ -9,5 +9,6 @@ export default async function BattleGroundPage({ params }: Props) {
   const resolved = await params
   const slug = resolved?.slug ?? []
   // slug->matchid/userid/problemId
+
   return <BattleGround matchId={slug[0]} userId={slug[1]} problemId={slug[2]} />
 }
