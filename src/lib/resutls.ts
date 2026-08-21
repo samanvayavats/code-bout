84f83c0c10d34c86cebb7eb84cd29d0236da6668
samanvaya <samanvayavats@gmail.com>
2026-08-21 21:47:49 +0530

    fixed the logic

--- src/lib/resutls.ts
+++ src/lib/resutls.ts
@@ -60,7 +60,7 @@ type User = {
 }
 
 export const getWinner = async (userOne: User, userTwo: User) => {
-  if (userOne.average_Verdict == userTwo.average_Verdict) {
+  if (userOne.average_Verdict === userTwo.average_Verdict) {
     userOne.pointCount += 1
     userTwo.pointCount += 1
   } else if (userOne.average_Verdict > userTwo.average_Verdict) {
@@ -71,31 +71,31 @@ export const getWinner = async (userOne: User, userTwo: User) => {
 
   if (userOne.average_exec_time_ms < userTwo.average_exec_time_ms) {
     userOne.pointCount += 1
-  } else {
+  } else if (userOne.average_exec_time_ms > userTwo.average_exec_time_ms) {
     userTwo.pointCount += 1
   }
 
   if (userOne.average_memory_kb < userTwo.average_memory_kb) {
     userOne.pointCount += 1
-  } else {
+  } else if (userOne.average_memory_kb > userTwo.average_memory_kb) {
     userTwo.pointCount += 1
   }
 
-  if (userOne.pointCount == userTwo.pointCount) {
+  if (userOne.pointCount === userTwo.pointCount) {
     const dateOne = new Date(userOne.submission_Time)
     const dateTwo = new Date(userTwo.submission_Time)
 
-    if (dateOne < dateTwo) {
+    if (dateOne.getTime() < dateTwo.getTime()) {
       userOne.pointCount += 1
     } else {
       userTwo.pointCount += 1
     }
   }
 
-  let winner_Id
-  let losser_Id
+  let winner_Id: string | undefined
+  let losser_Id: string | undefined
 
-  if (userOne.pointCount > userOne.pointCount) {
+  if (userOne.pointCount > userTwo.pointCount) {
     winner_Id = userOne.user_Id
     losser_Id = userTwo.user_Id
   } else {
