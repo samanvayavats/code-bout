40dac532abd934abab1e9c329b9bd8356da83b8d
samanvaya <samanvayavats@gmail.com>
2026-08-22 19:32:00 +0530

    added the session for the all the routed of leaderboard

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -1,8 +1,20 @@
 import prisma from '@/src/lib/prisma'
 import { NextRequest, NextResponse } from 'next/server'
-
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/src/lib/auth'
 export async function GET() {
   try {
+    const session = await getServerSession(authOptions)
+
+    if (!session) {
+      return NextResponse.json(
+        {
+          message: 'Invaild User ',
+        },
+        { status: 401 }
+      )
+    }
+
     const users = await prisma.user.findMany({
       orderBy: {
         wins: 'desc',
