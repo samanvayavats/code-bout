0ac051122b8896a08a9601c3c3377383980c0daf
samanvaya <samanvayavats@gmail.com>
2026-08-22 16:48:50 +0530

    fronted page for leaderboard

---
+++ src/app/leaderboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LeaderBoard from '@/src/components/ui/leader-board'
+const page = () => {
+  return (
+    <div>
+      <LeaderBoard />
+    </div>
+  )
+}
+
+export default page
